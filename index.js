bd60bde310c419ddca6254bb4db1f5b8173d5a8f
gnouy657-cell <gnouy657@gmail.com>
2026-07-08 20:46:50 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "eob2a41574f3at947904539fe34b012a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityNameElement = document.querySelector("#city-name-app");
   cityNameElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
