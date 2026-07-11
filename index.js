d76941f43f333dda0dae9edee027aac896e46c99
gnouy657-cell <gnouy657@gmail.com>
2026-07-11 17:38:31 -0400

    update temperature

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
 function displayWeatherCondition(response) {
-  console.log(response.data);
-
   let temperatureElement = document.querySelector("#temperature-value-app");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 
-  if (temperatureElement) {
-    temperatureElement.innerHTML = Math.round(
-      response.data.temperature.current,
-    );
-  } else {
-    console.log("Temperature element not found");
-  }
+  let cityNameElement = document.querySelector("#city-name-app");
+  cityNameElement.innerHTML = response.data.city;
+
+  let weatherDescriptionElement = document.querySelector(
+    "#weather-description-app",
+  );
+
+  weatherDescriptionElement.innerHTML = `
+    ${response.data.condition.description}
+    <br />
+    Humidity: <strong>${response.data.temperature.humidity}%</strong>
+    Wind: <strong>${response.data.wind.speed} km/h</strong>
+  `;
 }
 
 function searchCity(city) {
   let apiKey = "eob2a41574f3at947904539fe34b012a";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
 
-  axios
-    .get(apiUrl)
-    .then(displayWeatherCondition)
-    .catch(function (error) {
-      console.log("API error:", error);
-    });
+  axios.get(apiUrl).then(displayWeatherCondition);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#search-form-input");
-  let cityNameElement = document.querySelector("#city-name-app");
-
-  cityNameElement.innerHTML = searchInput.value;
 
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-
 searchFormElement.addEventListener("submit", handleSearchSubmit);
